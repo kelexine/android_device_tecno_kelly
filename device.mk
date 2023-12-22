2d1de0e78c9e1fa5fa5a9f69bdcce27b1013f303
kelexine <frankiekelechi@gmail.com>
2023-12-23 00:34:29 +0100

    KG5j : comment gsi keys

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 #
 
 # Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
