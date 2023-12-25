e28b02e366d860c76500a285134c44d5d399d244
kelexine <frankiekelechi@gmail.com>
2023-12-26 00:23:44 +0100

    KG5j : use prebuilt vendor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,6 +129,10 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Security patch level
 VENDOR_SECURITY_PATCH := 2023-08-01
 
+# Use prebuilt vendor
+BUILD_WITHOUT_VENDOR := true
+BOARD_PREBUILT_VENDORIMAGE := $(VENDOR_PATH)/vendor.img
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
