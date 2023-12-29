62a2a3eeb9632f3bf18a4d887a620a2fe59c3a75
kelexine <frankiekelechi@gmail.com>
2023-12-29 16:04:24 +0100

    KG5j : fix non-existent modules in device.mk

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,4 +5,4 @@ rm -rf device/mediatek/sepolicy_vndr
 # Clone Repos
 git clone https://github.com/nashc-dev/android_hardware_mediatek hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
-wget https://github.com/kelexine/android_device_tecno_KG5j/releases/download/v1/vendor.img vendor/tecno/KG5j/vendor.img
+wget https://github.com/kelexine/android_device_tecno_KG5j/releases/download/v1/vendor.img -O vendor/tecno/KG5j/vendor.img
