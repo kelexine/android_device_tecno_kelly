7715ff6fe3f577da836a80f1895293d6bb77264d
Franklin Kelechi <70432347+kelexine@users.noreply.github.com>
2024-01-12 09:31:34 +0100

    Change TARGET_ARCH_VARIANT to armv8-a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@ BOARD_USES_RECOVERY_AS_BOOT := true
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := generic
