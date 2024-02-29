a8ffcbbe9b7d24a9c0d9070ca8e48111083873c8
Franklin Kelechi <70432347+kelexine@users.noreply.github.com>
2024-02-29 07:45:23 +0100

    Comment drooling cloning

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,8 @@
 # Remove Existing Repos
-#rm -rf system/bpf
 #rm -rf hardware/mediatek
-rm -rf device/mediatek/sepolicy_vndr
+#rm -rf device/mediatek/sepolicy_vndr
 
 # Clone Repos
-#git clone https://github.com/kelexine/bpf system/bpf
 #git clone https://github.com/nashc-dev/android_hardware_mediatek hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-20 device/mediatek/sepolicy_vndr
+#git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-20 device/mediatek/sepolicy_vndr
 wget https://github.com/kelexine/android_device_tecno_KG5j/releases/download/v1/vendor.img -O vendor/tecno/KG5j/vendor.img
