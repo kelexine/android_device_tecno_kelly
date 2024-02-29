3ce1f07b4a1fe9b3ea1802cf5330d17f0e742dd7
Franklin Kelechi <70432347+kelexine@users.noreply.github.com>
2024-02-29 08:09:47 +0100

    sepolicy : Change mediatek SE policy to PE version

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,5 +4,5 @@ rm -rf device/mediatek/sepolicy_vndr
 
 # Clone Repos
 #git clone https://github.com/nashc-dev/android_hardware_mediatek hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-20 device/mediatek/sepolicy_vndr
+git clone https://github.com/PixelExperience/device_mediatek_sepolicy_vndr.git -b eleven device/mediatek/sepolicy_vndr
 wget https://github.com/kelexine/android_device_tecno_KG5j/releases/download/v1/vendor.img -O vendor/tecno/KG5j/vendor.img
